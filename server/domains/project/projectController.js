3fa670416cebd59fa4e4f2fd3356bde0405e66a2
RamiroBrayan456 <l181130435@gamadero.tecnm.mx>
2022-12-01 20:46:55 -0800

    Reto de validación

--- server/domains/project/projectController.js
+++ server/domains/project/projectController.js
@@ -1,37 +1,29 @@
-// Creando los Actions Methods RM
-// del controlador Project
-
-// GET "/project"
-// GET "/project/list"
 const list = (req, res) => {
-  // 1. Generando el view-model
   const viewModel = {};
-  // 2. Madamos a generar la vista con el Template Engine
   res.render('project/list', viewModel);
 };
 
-// GET "/project/add"
-// GET "/project/create"
 const showAddProjectForm = (req, res) => {
   const viewModel = {};
   res.render('project/add', viewModel);
 };
 
-// POST "/project/add"
-// POST "/project/create"
 const addProject = (req, res) => {
-  // Desesctructurando y renombrando error de datos
-  const { errorData: error } = req;
-  // Verificando si hay error de validación
+ 
+  const { validData, errorData: error } = req;
+  let project = {};
+  let errorModel = {};
   if (error) {
-    res.status(200).json(error);
+    project = error.value;
+    errorModel = error.inner.reduce((prev, curr) => {
+      const newVal = prev;
+      newVal[`${curr.path}Error`] = curr.message;
+      return newVal;
+    }, {});
   } else {
-    // Desestructurando datos del formulario
-    const { validData: projectData } = req;
-    // Contestando los datos del proyecti
-    res.status(200).json(projectData);
+    project = validData;
   }
+  res.status(200).render('project/add', { project, errorModel });
 };
 
-// Exportando el Controlador
 export default { list, showAddProjectForm, addProject };
